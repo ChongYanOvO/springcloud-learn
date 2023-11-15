f20501c75ee8e83d4b29586d98257fedf9608a18
chongyan <chongyan@163.com>
2023-11-15 18:33:03 +0800

    Hystrix and Gateway learn

--- service-borrow/src/main/java/com/chongyan/service/client/UserClient.java
+++ service-borrow/src/main/java/com/chongyan/service/client/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("service-user")
+@FeignClient(value = "service-user",fallback = UserFallbackClient.class)
 public interface UserClient {
     @RequestMapping("/user/{uid}")
     User getUserById(@PathVariable("uid") int uid);
